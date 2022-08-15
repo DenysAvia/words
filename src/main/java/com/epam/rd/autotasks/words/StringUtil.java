d7a48667598b931198937105dc9277183437e75c
Denys Kovika <d.v.kovika@gmail.com>
2022-08-15 11:53:22 +0300

    ver.1

--- src/main/java/com/epam/rd/autotasks/words/StringUtil.java
+++ src/main/java/com/epam/rd/autotasks/words/StringUtil.java
@@ -1,22 +1,108 @@
 package com.epam.rd.autotasks.words;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringUtil {
     public static int countEqualIgnoreCaseAndSpaces(String[] words, String sample) {
-        throw new UnsupportedOperationException();
+       // throw new UnsupportedOperationException();
+        if (sample == null || words == null)
+            return 0;
+
+        sample = sample.trim();
+        sample = sample.toLowerCase();
+        int count = 0;
+        for (String word : words) {
+            word = word.trim();
+            word = word.toLowerCase();
+            if (word.equals(sample))
+                count++;
+        }
+        return count;
     }
 
     public static String[] splitWords(String text) {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        if (text == null || text.equals(""))
+            return null;
+        text = text.replaceAll("\\s", "\\,");
+        text = text.replaceAll(";", "\\,");
+        text = text.replaceAll(":", "\\,");
+        text = text.replaceAll("\\.", "\\,");
+        text = text.replaceAll("\\?", "\\,");
+        text = text.replaceAll("!", "\\,");
+        String[] wordArr = new String[1000];
+        ArrayList<String> wordsList = new ArrayList<>();
+        wordArr = text.split(",");
+        for (String s : wordArr) {
+            if (!s.equals("")) {
+                wordsList.add(s);
+            }
+        }
+        wordArr = wordsList.toArray(new String[0]);
+        if (wordsList.isEmpty()) {
+            return null;
+        }
+        return wordArr;
     }
 
     public static String convertPath(String path, boolean toWin) {
-        throw new UnsupportedOperationException();
+       // throw new UnsupportedOperationException();
+        if(path == null){
+            return null;
+        }else if(path.equals("")) {
+            return null;
+        }else{
+            if(path.contains("C:") && path.lastIndexOf("C:\\") != 0 ){
+                return null;
+            }
+            if(path.contains("~") && path.lastIndexOf("~") != 0){
+                return null;
+            }
+            if(path.contains("/")&&path.contains("\\")) return null;
+            if(path.contains("C:")&&path.contains("/")) return null;
+            if(path.contains("~")&&path.contains("\\")) return null;
+
+
+            if(toWin){
+                path = path.replace("~","C:\\User");
+                if(path.indexOf("/")==0) {
+                    path = path.replaceFirst("/", "C:\\\\");// C:\\
+                }
+                path = path.replaceAll("/","\\\\");
+            }else{
+                path = path.replace("C:\\User","~");
+                path = path.replace("C:\\","/");
+                path = path.replaceAll("\\\\","/");
+            }
+            return path;
+        }
     }
 
     public static String joinWords(String[] words) {
-        throw new UnsupportedOperationException();
+       // throw new UnsupportedOperationException();
+        String res = "[";
+        ArrayList<String> list = new ArrayList<>();
+        if (words == null || words.length == 0)
+            return null;
+        for (String word : words)
+        {
+            if (!word.equals(""))
+                list.add(word);
+        }
+        if (list.size() == 0)
+            return null;
+        String lastChar = list.get(list.size()-1);
+        list.remove(list.size()-1);
+
+        for (String subList : list)
+        {
+            res = res + subList + ", ";
+        }
+        res += lastChar + "]";
+        return res;
     }
 
     public static void main(String[] args) {
